f73a92f14cf3be5225a20caf67f39e152c1c9e01
AidenH_123 <149191032+Stealthhacker123@users.noreply.github.com>
2024-03-23 02:39:40 +0000

    Added all js into script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+function showInput() {
+  var userInput = document.getElementById('userInput').value.toLowerCase(); ;
+
+  var x = ["stbg", "slime fighter", "cubey | official website", "facebook simulator", "gadgets", "secret docs"];
+  var y = 0;
+  var recorder = 0;
+  for(var i = 0; i < x.length; i++) {
+    recorder++;
+    if(userInput === x[i]) {
+      y = 1;
+      break;
+    }
+  }
+
+
+  if(y === 1) {
+    window.location.href = "#" + recorder;
+    document.getElementById("good_output").innerHTML = "Redirecting...";
+    elementToRemove.remove("bad_output");
+  }
+
+
+  else {
+    document.getElementById("bad_output").innerHTML = "Invalid Password. Please try again!";
+    element.remove("good_output");
+  }
+}
+
+function checkEnter(event) {
+  if(event.key === "Enter") {
+    showInput();
+  }
+}
+
+function hideDiv() {
+  document.getElementById('myDiv').style.display = 'none';
+}
